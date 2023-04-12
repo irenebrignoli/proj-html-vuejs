2b6ac917ba474dd4d838324ccbd11633ed38faa0
Irene Brignoli <ire.brignoli@gmail.com>
2023-04-12 10:32:31 +0200

    consultant card creata

---
+++ src/components/ConsultantsCard.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  name: "PageFooter",
+};
+</script>
+
+<template>
+  <div class="d-flex gap-5">
+    <div class="ms_icon flex-shrink-0">
+      <i class="fa-solid fa-chart-line"></i>
+    </div>
+    <div>
+      <h6 class="mb-3">Statistical cons</h6>
+      <p class="text_section">
+        When, while lovely valley teems with vapour around meand eridian sun
+        strikes the upper impenetrable foliage of my trees, and but a
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+.ms_icon {
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  background-color: $bg_darker_red;
+  text-align: center;
+
+  i {
+    color: $text-white;
+    line-height: 3rem;
+    font-size: 1.2rem;
+  }
+}
+
+h6 {
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 1.1rem;
+}
+</style>
--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,9 +1,50 @@
 <script>
+import ConsultantsCard from "../components/ConsultantsCard.vue";
+
 export default {
   name: "PageMain",
+  components: {
+    ConsultantsCard,
+  },
+  consultantsIcons: [
+    "fa-chart-line",
+    "fa-bullhorn",
+    "fa-piggy-bank",
+    "fa-bullseye",
+  ],
 };
 </script>
 
-<template></template>
+<template>
+  <!-- consultants section -->
+  <section id="consultants">
+    <div class="row justify-content-center">
+      <div class="col-8">
+        <h5>Our consultants con help you</h5>
+        <div class="row justify-content-between">
+          <div class="col-5">
+            <ConsultantsCard />
+          </div>
+          <div class="col-5"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+#consultants {
+  height: 50rem;
+  margin-top: 5rem;
+  color: $text_dark_gray;
+
+  h5 {
+    text-transform: uppercase;
+    font-weight: 700;
+    margin-bottom: 4.5rem;
+    font-size: 1.4rem;
+  }
+}
+</style>
